570143de311c60977dac7d9e67b17ecc1ca601e9
acp <prepodam@mail.ru>
2016-09-13 16:47:35 +0300

    Temporary disable consul-template.
    Unless exec option in new version.

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 consul-template -once -config=/etc/consul_template_initial.hcl
-timeout -t 10 consul-replicate -once -config /etc/consul_replicate_initial.hcl
-consul-template -config=/etc/consul_template.hcl
+consul-replicate -config /etc/consul_replicate_initial.hcl
