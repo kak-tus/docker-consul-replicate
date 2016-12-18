e8e8307960ed48ae9d7c1fceaf2452b01d090db7
Andrey Kuzmin <prepodam@mail.ru>
2016-12-18 15:19:06 +0300

    Send STD from child to docker.
    In background mode docker didn't receive STD from child.

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 consul-template -once -template="/etc/consul_replicate_initial.hcl.template:/etc/consul_replicate_initial.hcl"
 timeout -t 10 consul-replicate -config /etc/consul_replicate_initial.hcl
 
-consul-template -config="/etc/consul_template.hcl" &
+consul-template -config="/etc/consul_template.hcl" >/proc/1/fd/1 2>/proc/1/fd/2 &
 child=$!
 
 trap "kill $child" SIGTERM
